f3582f5b9ccfde63c6976a34b38b220e31b85caa
Tor S. Haugland <tor.haugland@sintef.no>
2024-07-02 09:31:46 +0200

    Add example data

--- SEM/datamodels/main.py
+++ SEM/datamodels/main.py
@@ -1,22 +1,23 @@
 from pathlib import Path
 import dlite
-import glob
 
 thisdir = Path(__file__).resolve().parent
 dlite.python_storage_plugin_path.append(thisdir  / "plugins")
 
 # ThermoFischer
-inst1 = dlite.Instance.from_location("ThermoFischerStorage", "(ThermoFischer) pos1_01_grid_200x.tif")
+fname1 = Path("example_data/(ThermoFischer) pos1_01_grid_200x.tif")
+inst1 = dlite.Instance.from_location("ThermoFischerStorage", fname1)
 print(inst1)
 
 # Hitachi
-inst2 = dlite.Instance.from_location("HitachiStorage", "(Hitachi) 15_m001.tif")
+fname2 = Path("example_data/(Hitachi) 15_m001.tif")
+inst2 = dlite.Instance.from_location("HitachiStorage", fname2)
 print(inst2)
 
 # To test this, change to your corresponding path to SEM_cement_batch2
-cement_batch2_files = glob.glob("/mnt/c/Users/torha/SINTEF/HEU MatCHMaker - Dokumenter/WP2/SEM_cement_batch2/77600-23-001/*.tif")
-with dlite.Storage("pyrdf", location="SEM_cement_batch2.rdf", options="mode=w;single=no") as s:
-    for fname in cement_batch2_files:
-        print(fname)
-        inst = dlite.Instance.from_location("HitachiStorage", fname)
-        s.save(inst)
+#cement_batch2_files = glob.glob("/mnt/c/Users/torha/SINTEF/HEU MatCHMaker - Dokumenter/WP2/SEM_cement_batch2/77600-23-001/*.tif")
+#with dlite.Storage("pyrdf", location="SEM_cement_batch2.rdf", options="mode=w;single=no") as s:
+#    for fname in cement_batch2_files:
+#        print(fname)
+#        inst = dlite.Instance.from_location("HitachiStorage", fname)
+#        s.save(inst)
